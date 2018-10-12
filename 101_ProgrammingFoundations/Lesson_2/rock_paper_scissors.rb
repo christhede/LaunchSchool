658261fe596502a6debc5dc1be12b0fb7b489409
Chris Thede <christhede17@gmail.com>
2018-10-12 18:59:41 -0400

    Oct. 12

--- 101_ProgrammingFoundations/Lesson_2/rock_paper_scissors.rb
+++ 101_ProgrammingFoundations/Lesson_2/rock_paper_scissors.rb
@@ -1,15 +1,7 @@
 VALID_CHOICES = %w(Rock Paper Scissors Lizard Spock)
 
-def game_score(input)
-computer_score = []
-player_score = []
-  if input == 1
-    player_score << 1
-  elsif 
-    input == 2
-    computer_score << 1
-  end
-  prompt("the score is computer:#{computer_score}, player:#{player_score}")
+def prompt(message)
+  puts "=> #{message}"
 end
 
 def win?(first, second)
@@ -28,62 +20,68 @@ end
 def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
-    game_score(1)
-  elsif 
-    win?(computer, player)
+  elsif win?(computer, player)
     prompt("Computer won.")
-    game_score(2)
   else
     prompt("It's a tie")
   end
 end
 
-def prompt(message)
-  puts "=> #{message}"
-end
+computer_score = []
+player_score = []
 
 loop do
-  choice = ""
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    choice = ""
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
-      break
-    elsif choice.downcase.start_with?("s")
-      prompt("do you mean Spock or Scissors?")
-      s_answer = gets.chomp
-        if s_answer == "Spock"
-          choice = "Spock"
-          break
-        else 
-          choice = "Scissors"
-          break
-        end
-    elsif choice.downcase.start_with?("p")
-      choice = "Paper"
-      break
-    elsif choice.downcase.start_with?("l")
-      choice = "Lizard"
-      break
-    elsif choice.downcase.start_with?("r")
-      choice = "Rock"
-      break
-    else
-      prompt("That's not a valid choice.")
+      if VALID_CHOICES.include?(choice)
+        break
+      elsif choice.downcase.start_with?("s")
+        prompt("do you mean Spock or Scissors?")
+        s_answer = gets.chomp
+          if s_answer == "Spock"
+            choice = "Spock"
+            break
+          else 
+            choice = "Scissors"
+            break
+          end
+      elsif choice.downcase.start_with?("p")
+        choice = "Paper"
+        break
+      elsif choice.downcase.start_with?("l")
+        choice = "Lizard"
+        break
+      elsif choice.downcase.start_with?("r")
+        choice = "Rock"
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{choice}, computer chose #{computer_choice}.")
+
+    score = display_results(choice, computer_choice)
+
+    if score == "Computer won."
+      computer_score << 1
+    elsif score == "You won!"
+      player_score << 1
+    end
+
+    prompt("The score is => Computer: #{computer_score.sum}, Player: #{player_score.sum}")
+    break if computer_score == 5 || player_score == 5
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose #{choice}, computer chose #{computer_choice}.")
-
-  display_results(choice, computer_choice)
 
   prompt("Do you want to play again? Y or N")
   answer = gets.chomp.downcase
   break unless answer.downcase.end_with?("y")
 end
-
 
 prompt("Thank you for playing, goodbye")
