8885d52db6aaf8614f935bf037ddb506c3892d57
Chris Thede <christhede17@gmail.com>
2018-10-14 23:10:15 -0400

    Simplified win? method with wins variable

--- 101_ProgrammingFoundations/Lesson_2/rock_paper_scissors.rb
+++ 101_ProgrammingFoundations/Lesson_2/rock_paper_scissors.rb
@@ -1,5 +1,11 @@
 VALID_CHOICES = %w(Rock Paper Scissors Lizard Spock)
 
+WINS = { "Scissors": ["Paper", "Lizard"],
+         "Paper": ["Rock", "Spock"],
+         "Rock": ["Lizard", "Paper"],
+         "Spock": ["Scissors", "Rock"],
+         "Lizard": ["Spock", "Paper"] }
+
 computer_score = 0
 player_score = 0
 choice = ""
@@ -10,16 +16,11 @@ def prompt(message)
 end
 
 def win?(first, second)
-  (first == "Scissors" && second == "Paper") ||
-    (first == "Paper" && second == "Rock") ||
-    (first == "Rock" && second == "Lizard") ||
-    (first == "Lizard" && second == "Spock") ||
-    (first == "Spock" && second == "Scissors") ||
-    (first == "Scissors" && second == "Lizard") ||
-    (first == "Lizard" && second == "Paper") ||
-    (first == "Paper" && second == "Spock") ||
-    (first == "Spock" && second == "Rock") ||
-    (first == "Rock" && second == "Scissors")
+  (first == "Rock" && second == WINS[:Rock]) ||
+    (first == "Paper" && second == WINS[:Paper]) ||
+    (first == "Scissors" && second == WINS[:Scissors]) ||
+    (first == "Lizard" && second == WINS[:Lizard]) ||
+    (first == "Spock" && second == WINS[:Spock])
 end
 
 loop do
@@ -33,7 +34,6 @@ loop do
       elsif choice.downcase == "s"
         prompt("Please choose Spock or Scissors:")
         s_choice = gets.chomp.downcase
-        # update s_choice to loop
         if s_choice == "spock"
           choice = "Spock"
           break
@@ -43,7 +43,6 @@ loop do
         else
           prompt("That is not a valid choice. Please choose Spock or Scissors")
         end
-
       elsif choice.downcase == "p"
         choice = "Paper"
         break
@@ -72,13 +71,13 @@ loop do
       prompt("It's a tie")
     end
 
-    prompt("The score is => Computer: #{computer_score}, Player: #{player_score}")
+    prompt("Computer: #{computer_score}, Player: #{player_score}")
 
     if computer_score == 5
-      prompt("The computer won with a score of #{computer_score}-#{player_score}")
+      prompt("The computer won the match #{computer_score}-#{player_score}")
       break
     elsif player_score == 5
-      prompt("The player won with a score of #{player_score}-#{computer_score}")
+      prompt("The player won the match #{player_score}-#{computer_score}")
       break
     end
   end
