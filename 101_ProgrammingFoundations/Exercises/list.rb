57f689aa3484be4d388e4cd2aa6caeb26814a983
Chris Thede <christhede17@gmail.com>
2018-11-21 21:38:23 -0500

    Nov 21

---
+++ 101_ProgrammingFoundations/Exercises/list.rb
@@ -0,0 +1,37 @@
+customer_orders = [
+  {
+    customer_id: 12,
+    customer_name: 'Emma Lopez',
+    orders: [
+      { order_fulfilled: true, order_value: 135.99 },
+      { order_fulfilled: true, order_value: 289.49 },
+      { order_fulfilled: false, order_value: 58.00 }
+    ]
+  },
+  {
+    customer_id: 32,
+    customer_name: 'Michael Richards',
+    orders: [
+      { order_fulfilled: true, order_value: 120.00 },
+      { order_fulfilled: false, order_value: 85.65 }
+    ]
+  },
+  # rest of data...
+]
+
+fulfilled_orders = customer_orders.map do |customer|
+  {
+    customer_id: customer[:customer_id],
+    customer_name: customer[:customer_name]
+  }
+end
+
+customer_orders.each_with_index do |data, index|
+  order_value = data[:orders].reduce(0) do |total, order|
+    total + order[:order_value] if order[:order_fulfilled] == true
+  end
+
+  fulfilled_orders[index][:order_value] = order_value
+end
+
+puts fulfilled_orders
