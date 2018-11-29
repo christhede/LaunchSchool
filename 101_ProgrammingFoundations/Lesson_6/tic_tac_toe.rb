c594d24ce5e4c1b097d10c22b357f28a0817b498
Chris Thede <christhede17@gmail.com>
2018-11-28 23:05:18 -0500

    COULD NOT FIGURE OUT AI DEFENSE, SO CLOSE

--- 101_ProgrammingFoundations/Lesson_6/tic_tac_toe.rb
+++ 101_ProgrammingFoundations/Lesson_6/tic_tac_toe.rb
@@ -79,14 +79,12 @@ end
 
 def computer_places_piece_defense!(brd)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 2
-      brd.values_at(*line).map do |x|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2 &&
+       brd.values_at(*line).include?(INITIAL_MARKER)
+       # brd = COMPUTER_MARKER
        binding.pry
-        if x == INITIAL_MARKER
-          brd[x] = COMPUTER_MARKER
-          binding.pry
-        end
-      end
+      #   end
+      # end
     # else
     #   square = empty_squares(brd).sample
     #   brd[square] = COMPUTER_MARKER
