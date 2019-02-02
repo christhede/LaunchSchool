a7308e7a36364ba9fa308b6f06a9e4736785870c
Slim Thede <christhede17@gmail.com>
2019-02-02 12:51:18 -0500

    Feb 2

---
+++ 101_ProgrammingFoundations/Exercises/fib.rb
@@ -0,0 +1,21 @@
+# Write a recursive method that computes the nth Fibonacci number, where nth is an argument to the method.
+
+# create an array of fib numbers
+# if n == 7, 7 is the 8th index in the array of fibonacci numbers
+# iterate through numbers, starting at 1
+# use the times method for how many times it will iterate
+
+def fib(n)
+    
+end
+
+fib(8)
+
+# fib(8) = 21
+# fibonacci(1) == 1
+# fibonacci(2) == 1
+# fibonacci(3) == 2
+# fibonacci(4) == 3
+# fibonacci(5) == 5
+# fibonacci(12) == 144
+# fibonacci(20) == 6765
