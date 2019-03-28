d2bf53f8d19c875133843b395e6a4d83ee7ae3dc
Slim Thede <christhede17@gmail.com>
2019-03-28 19:09:23 -0400

    March 28

--- 120_Object_Oriented_Programming/Lesson_5/twenty_one.rb
+++ 120_Object_Oriented_Programming/Lesson_5/twenty_one.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
+module Tools
+  def clear
+    system('clear')
+  end
+
+  def prompt(message)
+    puts "=> #{message}"
+  end
+end
+
 
 class Participant
   attr_accessor :hand, :total
@@ -18,6 +28,10 @@ class Participant
 
   def busted?
     self.total > 21
+  end
+
+  def blackjack?
+    self.total == 21
   end
 
   def update_total
@@ -76,25 +90,35 @@ class Deck
 end
 
 class TwentyOne
+  include Tools
+
+  LINE_BREAK = '-----------------'
   attr_accessor :player, :dealer, :deck
 
   def initialize
     @deck = Deck.new
-    @player = Player.new('chris')
+    @player = Player.new('Chris')
     @dealer = Dealer.new('Dealer Jim')
   end
 
   def start
+    clear
+    display_welcome_message
     deal_cards
-    show_initial_cards
-    # player_turn
-    # dealer_turn
-    # show_result
+    display_cards
+    player_turn
+    dealer_turn unless player.busted? || player.blackjack?
+    show_results
   end
 
   def choose_player_name
-    puts "What is your name?"
+    prompt "What is your name?"
     gets.chomp.capitalize
+  end
+
+  def display_welcome_message
+    prompt "Welcome to Twenty One #{player.name}."
+    # sleep(1)
   end
 
   def deal_card
@@ -102,43 +126,119 @@ class TwentyOne
   end
 
   def deal_cards
+    prompt "Dealing cards..."
+    # sleep(1)
+    clear
     2.times {dealer.hand << deal_card}
     2.times {player.hand << deal_card}
+  end
+
+  def update_total
     player.update_total
+    dealer.update_total
   end
 
-  def show_initial_cards
-    puts "#{player.name} has a #{player.hand}"
-    # puts "#{player.name} has a #{join(player.hand[0])} and a #{join(player.hand[1])}"
-    puts "#{dealer.name} is showing a #{join(dealer.hand[0])}"
-    # player.update_total
-    p player.total
+  def display_cards
+    clear
+    update_total
+    prompt "#{player.name} is holding #{joinand(player.hand)}"
+    prompt "#{dealer.name} is showing #{joinand(dealer.hand[0])}"
+    puts LINE_BREAK
+    prompt "#{player.name} hand total: #{player.total}"
+    prompt "#{dealer.name} top card total: #{dealer.hand[0][0]}"
+    puts LINE_BREAK
   end
 
-  def join(array)
-    array.join(' of ')
+  def display_final_flop
+    clear
+    update_total
+    prompt "Final flop:"
+    prompt "#{player.name} is holding #{joinand(player.hand)}."
+    prompt "#{dealer.name} is holding #{joinand(dealer.hand)}."
+    puts LINE_BREAK
+    prompt "#{player.name} hand total: #{player.total}"
+    prompt "#{dealer.name} hand total: #{dealer.total}"
+    puts LINE_BREAK
   end
 
-  def player_turn
-    puts "Would you like to hit or stay?"
-    answer = gets.chomp.downcase
-    if player.busted?
-      puts "#{player.name} bust!"
-    elsif answer == 'hit'
-      player.add_card_to_hand(deal_card)
+  def joinand(array)
+    # binding.pry
+    if array.flatten.size == 4
+      array.map {|card| "a #{card.join(' of ')}"}.join(' and ')
+    elsif array.flatten.size == 2
+      "a #{array.join(' of ')}"
     else
-      puts "#{player.name} chose to hit"
+      array.map { |card|
+        if card == array.last
+          "and a #{card.join(' of ')}"
+        else
+          "a #{card.join(' of ')}, "
+        end
+      }.join
     end
   end
 
+  def player_turn
+    loop do
+      prompt "Would you like to hit or stay?"
+      answer = gets.chomp.downcase
+      if player.total == 21
+        prompt "Blackjack!"
+      elsif ['hit', 'h'].include? answer
+        player_hit
+      elsif ['stay', 's'].include? answer
+        prompt "#{player.name} chose to stay"
+      else
+        prompt "That is not a valid answer. Please choose hit, or stay"
+      end
+
+      break if answer == 'stay' || player.busted? || player.blackjack?
+    end
+    sleep(1)
+  end
+
+  def player_hit
+    prompt "#{player.name} chose to hit"
+    player.add_card_to_hand(deal_card)
+    display_cards
+  end
+
   def dealer_turn
-    if dealer.busted?
-      puts "Dealer busted!"
-    elsif dealer.hand >= 17
-      puts "#{dealer.name} chose to stay"
+    clear
+    display_cards
+    unless dealer.busted? || dealer.total >= 17 && dealer.total >= player.total
+      dealer_hit
+    end
+  end
+
+  def dealer_hit
+    prompt "#{dealer.name} chose to hit"
+    dealer.add_card_to_hand(deal_card)
+    sleep(1)
+  end
+
+  def show_results
+    display_final_flop
+    if player.busted?
+      prompt "#{player.name} busted!"
+    elsif dealer.busted?
+      prompt "#{dealer.name} busted!"
+    elsif player.blackjack?
+      prompt "#{player.name} Blackjack!"
+    elsif dealer.blackjack?
+      prompt "#{dealer.name} Blackjack!"
     else
-      puts "#{dealer.name} chose to hit"
-      dealer.add_card_to_hand(deal_card)
+      total_comparison
+    end
+  end
+
+  def total_comparison
+    if player.total == dealer.total
+      prompt "It's a tie"
+    elsif player.total > dealer.total
+      prompt "#{player.name} won!"
+    else
+      prompt "#{dealer.name} won!"
     end
   end
 end
