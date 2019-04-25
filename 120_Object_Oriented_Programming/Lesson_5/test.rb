3b2990f36ffbb7f970b60deb85468bb2ba9fd84d
Slim Thede <christhede17@gmail.com>
2019-04-24 20:04:55 -0400

    April 24

--- 120_Object_Oriented_Programming/Lesson_5/test.rb
+++ 120_Object_Oriented_Programming/Lesson_5/test.rb
@@ -1,64 +1,19 @@
-def cycle(n)
-  if coprime?(divisible_array(10), divisible_array(n))
-    repeating_pattern(1000**100/n)
-  else
-    -1
-  end
+# For building the encrypted string:
+# Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+# Do this n times!
+
+# input: a string
+# first, iterate through string, pushing every second char to new_string
+# then join original string and new_string together with #join
+# do this n times with n.times
+
+# output: a new string
+
+def encrypt(text, n)
+  new_string = ''
+  text.chars.each_with_index { |char, idx| new_string << char if idx % 2 == 0 }
+  puts new_string
+  puts text
 end
 
-def divisible_array(num)
-  arr = []
-  (2..num-1).each do |x|
-    arr << x if num % x == 0
-  end
-
-  arr
-end
-
-def coprime?(array1, array2)
-  !array1.any? { |x| array2.include? x }
-end
-
-def repeating_pattern(num)
-  zeros = num.to_s.chars.size - 2
-  number = '1' + '0' * zeros
-  num *= number.to_i
-  num = num.to_i
-
-  arr = num.digits.reverse
-  
-  counter = 1
-  counter_two = 0
-  new_arr = []
-
-  loop do
-    until arr[0] == arr[counter]
-      counter += 1
-    end
-    
-    new_arr = []
-    counter_two = counter
-
-    counter.times do |x|
-      if arr[x] == arr[counter_two]
-        new_arr << arr[x]
-        counter_two += 1
-      else
-        break
-      end
-    end
-
-    break if new_arr.size == counter
-
-    counter += 1
-  end
-
-  new_arr.size
-end
-
-p cycle(33) # 2
-p cycle(18118) # -1
-p cycle(69) # 22
-p cycle(197) # 98
-p cycle(65) # -1
-# return number of numbers repeating
+encrypt("This is a test!", 0)
