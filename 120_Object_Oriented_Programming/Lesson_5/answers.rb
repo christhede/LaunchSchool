7f45ae05c0ddbb427a4745e0107230df1330b175
Slim Thede <christhede17@gmail.com>
2019-04-27 13:54:44 -0400

    April 26

--- 120_Object_Oriented_Programming/Lesson_5/answers.rb
+++ 120_Object_Oriented_Programming/Lesson_5/answers.rb
@@ -26,3 +26,25 @@ doggo.breathes
 What is an object?
 Everything in ruby is an object. They can be an integer, array, hash and so on. Objects are tools to store information from a class. Objects are instances of classes.
 You can create objects by calling the method 'new' on the class. Objects can have the same behaviours, but differnent states. 
+
+What is a class? 
+A class in ruby is a blueprint for objects. They hold certain behaviors  Objects are created from classes. 
+
+What does lexical scope mean? 
+Lexical scope is a term used to describe the scope of an object that remains limited to where it was defined in the code. Specifically constants in 
+ruby use lexical scope, and have differnt scoping rules from other variables, especially apparent in the case of inheritance.
+
+What are Modules?
+Modules are a way adding in behaviours to classes. Usually if multiple classes can use the same behaviours or methods, we use modules to consolidate
+all the methods needed and add them into the specified class with the 'include' keyword. 
+
+Method Lookup Path
+The method lookup path is the order in which ruby will look for the method that is being called. You can find the visual lookup path with the 
+method ancestors. method::ancestors will print out the lookup path, ending in Object
+
+What is self? 
+Self is a keyword used to do two things in ruby. When self is seen outside of a method, it is referencing the class itself. Such as a class method. 
+A class method can only be called from the class itself. When self is seen inside of an instance method, it is referencing an accessor method. 
+
+# example:
+
